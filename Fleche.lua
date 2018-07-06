770ca4490d6b10554caefd859e831a4398955ce4
Hugo Valente <hugo.valente1996@gmail.com>
2018-07-06 07:00:18 +0000

    CrÃéle programme qui fait les flÃches

--- Fleche.lua
+++ Fleche.lua
@@ -1,44 +1,25 @@
 #!/usr/bin/lua5.3
 -- Dessine une fleche sur la console
--- hv 180705.1540
+-- hv 180706.0858
 -- source:
 
 taille_fleche=arg[1]
 
--- bloc pointe de la fleche
-for i=1, taille_fleche do io.write(" ") end
-print("*")
+-- pointe de la fleche
+for i=1, taille_fleche do io.write(" ") end print("*")
 
--- bloc de la tete de la fleche
-
+-- tete de la fleche
 for j=1, taille_fleche-2 do
-	for i=1, taille_fleche-j-1 do
-        	io.write(" ")		
-        end
-	io.write("*")
-	
-	for g=1, 2*j+1 do
-		io.write(" ")
-	end
-	print("*")
+	for i=1, taille_fleche-j-1 do io.write(" ") end io.write("*")
+	for g=1, 2*j+1 do io.write(" ")	end print("*")
 end
 
+-- bas de la tete de la fleche
+for i=1, taille_fleche*2 do io.write("*")end print("*")
 
-
-
--- bloc du bas de la tete de la fleche
-
-for i=1, taille_fleche*2 do
-	io.write("*")
-end
-print("*")
-
--- bloc de la queue de la fleche
+-- queue de la fleche
 for j=1, taille_fleche do
-for i=1, taille_fleche do
-	io.write(" ")
-end 
-print("*")
+	for i=1, taille_fleche do io.write(" ")end print("*")
 end
 
 
--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 # !/bin/bash
 # Script pour lancer le programme
-# hv 180705.1541
+# hv 180706.0840
 
-./Fleche.lua 11
+./Fleche.lua 3
+./Fleche.lua 6
+./Fleche.lua 9
+
 
