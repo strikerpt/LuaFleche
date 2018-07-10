44319342ce8b746cef191833f68a8c06f071c4ab
Hugo Valente <hugo.valente1996@gmail.com>
2018-07-10 09:12:20 +0000

    kata fleche en lua

--- Fleche.lua
+++ Fleche.lua
@@ -1,22 +1,24 @@
 #!/usr/bin/lua5.3
 -- Dessine une fleche sur la console
--- hv 180706.1059
+-- hv 180710.1106
 -- source:
 
-taille_fleche= tonumber(arg[1])
+taille_fleche=tonumber(arg[1])
+if taille_fleche and (taille_fleche>=2) and (taille_fleche<=20) then
+	for i=1, 2*taille_fleche do
+		-- pointe de la fleche
+		if i==1 then print(string.rep(" ",taille_fleche).."*")
 
-for i=1, 2*taille_fleche do 
--- pointe de la fleche
-	if i==1 then print(string.rep(" ",taille_fleche).."*")
-	
--- tete de la fleche
-	elseif i==2 or i<taille_fleche then print(string.rep(" ",taille_fleche-i).."*"..string.rep(" ",2*i-1).."*")
+		-- tete de la fleche
+		elseif i==2 or i<taille_fleche then print(string.rep(" ",taille_fleche-i).."*"..string.rep(" ",2*i-1).."*")
 
--- bas de la tete de la fleche
-	elseif i==taille_fleche then print(string.rep("*",taille_fleche*2+1))
+		-- bas de la tete de la fleche
+		elseif i==taille_fleche then print(string.rep("*",taille_fleche*2+1))
 
--- queue de la fleche
-	elseif i>taille_fleche then print(string.rep(" ",taille_fleche).."*")
+		-- queue de la fleche
+		elseif i>taille_fleche then print(string.rep(" ",taille_fleche).."*")
+		end
 	end
+else
+		print("Choisissez un nombre entre 2 et 20")
 end
-
