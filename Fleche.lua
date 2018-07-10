965535369d4fba9a3eb6c670eb8a529af41ff3e7
strikerpt <39523323+strikerpt@users.noreply.github.com>
2018-07-10 11:21:22 +0200

    Update Fleche.lua

--- Fleche.lua
+++ Fleche.lua
@@ -1,21 +1,21 @@
 #!/usr/bin/lua5.3
--- Dessine une fleche sur la console
+-- Dessine une flèche sur la console
 -- hv 180710.1106
 -- source:
 
 taille_fleche=tonumber(arg[1])
 if taille_fleche and (taille_fleche>=2) and (taille_fleche<=20) then
 	for i=1, 2*taille_fleche do
-		-- pointe de la fleche
+		-- pointe de la flèche
 		if i==1 then print(string.rep(" ",taille_fleche).."*")
 
-		-- tete de la fleche
+		-- tete de la flèche
 		elseif i==2 or i<taille_fleche then print(string.rep(" ",taille_fleche-i).."*"..string.rep(" ",2*i-1).."*")
 
-		-- bas de la tete de la fleche
+		-- bas de la tete de la flèche
 		elseif i==taille_fleche then print(string.rep("*",taille_fleche*2+1))
 
-		-- queue de la fleche
+		-- queue de la flèche
 		elseif i>taille_fleche then print(string.rep(" ",taille_fleche).."*")
 		end
 	end
